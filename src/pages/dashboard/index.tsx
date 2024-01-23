c76d21b05d01a66f62b92d83801199f59fa6a5e8
harshalkoli123 <harshalyesikar1713@gmail.com>
2024-01-23 12:00:47 +0530

    code push

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -27,220 +27,225 @@ const Dashboard = () => {
     setAge(event.target.value)
   }
 
- 
-
   return (
     <>
-           <Card style={{ width:'100%',borderRadius:"10px"}}>
-            <CardContent >
-              <h3 style={{borderBottom:"1px solid gray", margin:"20px" }}>Your Bussiness</h3>
-              <List component='nav' aria-label='main mailbox' sx={{}}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Bussiness Details' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Services' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Packages' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Membership' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Calender Settings' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Calender Fields' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Constent Forms' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Booking Settings' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Multiple location' />
-                  </ListItemButton>
-                </ListItem>
-              </List>
-              <Divider sx={{ marginTop:-5 }} />
-              <h3 style={{borderBottom:"1px solid gray",margin:"20px"}}>Staff Setting </h3>
-              <List component='nav' aria-label='secondary mailbox' sx={{ width: '500px',marginBottom:"15px",marginTop:"-20px" }}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Manage Staff' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Commission' />
-                  </ListItemButton>
-                </ListItem>
+      <Card style={{ width: '100%', borderRadius: '10px' }}>
+        <CardContent>
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Your Bussiness</h3>
+          <List component='nav' aria-label='main mailbox' sx={{}}>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Bussiness Details' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Services' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Packages' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Membership' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Calender Settings' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Calender Fields' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Constent Forms' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Booking Settings' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Multiple location' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider sx={{ marginTop: -5 }} />
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Staff Setting </h3>
+          <List
+            component='nav'
+            aria-label='secondary mailbox'
+            sx={{ width: '500px', marginBottom: '15px', marginTop: '-20px' }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Manage Staff' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Commission' />
+              </ListItemButton>
+            </ListItem>
 
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary='Role' />
-                  </ListItemButton>
-                </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Role' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider sx={{ marginTop: -5 }} />
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Stock </h3>
+          <List
+            component='nav'
+            aria-label='secondary mailbox'
+            sx={{ width: '500px', marginBottom: '15px', marginTop: '-20px' }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Products' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Supplier' />
+              </ListItemButton>
+            </ListItem>
 
-              </List>
-              <Divider sx={{ marginTop:-5 }} />
-              <h3 style={{borderBottom:"1px solid gray",margin:"20px"}}>Stock </h3>
-              <List component='nav' aria-label='secondary mailbox' sx={{ width: '500px',marginBottom:"15px",marginTop:"-20px" }}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Products' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Supplier' />
-                  </ListItemButton>
-                </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Inventory Setting ' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider sx={{ marginTop: -5 }} />
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Notification</h3>
+          <List
+            component='nav'
+            aria-label='secondary mailbox'
+            sx={{ width: '500px', marginBottom: '15px', marginTop: '-20px' }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Sent Messages' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Customer Messages' />
+              </ListItemButton>
+            </ListItem>
 
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary='Inventory Setting ' />
-                  </ListItemButton>
-                </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='WhatsApp Templates ' />
+              </ListItemButton>
+            </ListItem>
 
-              </List>
-              <Divider sx={{ marginTop:-5 }} />
-              <h3 style={{borderBottom:"1px solid gray",margin:"20px"}}>Notification</h3>
-              <List component='nav' aria-label='secondary mailbox' sx={{ width: '500px',marginBottom:"15px",marginTop:"-20px" }}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Sent Messages' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Customer Messages' />
-                  </ListItemButton>
-                </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary=' Customer Emails' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Notifications' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider sx={{ marginTop: -5 }} />
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Pramote</h3>
+          <List
+            component='nav'
+            aria-label='secondary mailbox'
+            sx={{ width: '500px', marginBottom: '15px', marginTop: '-20px' }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Sent Messages' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Customer Messages' />
+              </ListItemButton>
+            </ListItem>
 
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary='WhatsApp Templates ' />
-                  </ListItemButton>
-                </ListItem>
-                
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary=' Customer Emails' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary='Notifications' />
-                  </ListItemButton>
-                </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='WhatsApp Templates ' />
+              </ListItemButton>
+            </ListItem>
 
-              </List>
-              <Divider sx={{ marginTop:-5 }} />
-              <h3 style={{borderBottom:"1px solid gray",margin:"20px"}}>Pramote</h3>
-              <List component='nav' aria-label='secondary mailbox' sx={{ width: '500px',marginBottom:"15px",marginTop:"-20px" }}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Sent Messages' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Customer Messages' />
-                  </ListItemButton>
-                </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary=' Customer Emails' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Notifications' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider sx={{ marginTop: -5 }} />
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Point Of Sale</h3>
+          <List
+            component='nav'
+            aria-label='secondary mailbox'
+            sx={{ width: '500px', marginBottom: '15px', marginTop: '-20px' }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Payments Methods' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Tax Setting' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Other Setting ' />
+              </ListItemButton>
+            </ListItem>
 
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary='WhatsApp Templates ' />
-                  </ListItemButton>
-                </ListItem>
-                
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary=' Customer Emails' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary='Notifications' />
-                  </ListItemButton>
-                </ListItem>
-
-              </List>
-              <Divider sx={{ marginTop:-5 }} />
-              <h3 style={{borderBottom:"1px solid gray",margin:"20px"}}>Point Of Sale</h3>
-              <List component='nav' aria-label='secondary mailbox' sx={{ width: '500px',marginBottom:"15px",marginTop:"-20px" }}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Payments Methods' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Tax Setting' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Other Setting ' />
-                  </ListItemButton>
-                </ListItem>
-                
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                
-                    <ListItemText primary=' Bill Settings' />
-                  </ListItemButton>
-                </ListItem>
-                
-
-              </List>
-              <Divider sx={{ marginTop:-5 }} />
-              <h3 style={{borderBottom:"1px solid gray",margin:"20px"}} >Security</h3>
-              <List component='nav' aria-label='secondary mailbox' sx={{ width: '500px',marginBottom:"15px",marginTop:"-20px" }}>
-                <ListItem disablePadding>
-                  <ListItemButton>
-                    <ListItemText primary='Google Authentication' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem disablePadding>
-                  <ListItemButton component='a' href='#simple-list'>
-                    <ListItemText primary='Log Activity' />
-                  </ListItemButton>
-                </ListItem>
-              </List>
-           
-            </CardContent>
-          </Card>
-        
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary=' Bill Settings' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider sx={{ marginTop: -5 }} />
+          <h3 style={{ borderBottom: '1px solid gray', margin: '20px' }}>Security</h3>
+          <List
+            component='nav'
+            aria-label='secondary mailbox'
+            sx={{ width: '500px', marginBottom: '15px', marginTop: '-20px' }}
+          >
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary='Google Authentication' />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component='a' href='#simple-list'>
+                <ListItemText primary='Log Activity' />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </CardContent>
+      </Card>
     </>
   )
 }
